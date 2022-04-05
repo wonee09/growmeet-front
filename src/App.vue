0fce73b37c44013121db611233a6fb14d970a07f
wonee09 <wonjang031@gmail.com>
2022-04-05 20:13:27 +0900

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <main-image-view></main-image-view>
+  </div>
+</template>
+
+<script>
+import MainImageView from './components/MainImageView.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MainImageView
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MainImageView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="main_image_div">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaPglR3wA8x769-lSRx03lIl0KbgsbXdYtDw&usqp=CAU" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.main_image_div {
+    width: 100%;
+    height: fit-content;
+    background: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaPglR3wA8x769-lSRx03lIl0KbgsbXdYtDw&usqp=CAU');
+    /* background-size:contain; */
+}
+
+.main_image_div>img {
+    width: 100%;
+    /* opacity : 0; */
+    /* z-index: -1; */
+}
+
+
+</style>
